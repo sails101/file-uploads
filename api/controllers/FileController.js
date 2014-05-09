33b556a40ce105b278eb89a6d0257ff9247e8b25
Mike McNeil <mike@balderdash.co>
2014-05-09 01:09:53 -0500

    Added streaming upload to S3 example

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -23,6 +23,29 @@ module.exports = {
     });
   },
 
+  /**
+   * `FileController.s3upload()`
+   */
+  s3upload: function (req, res) {
+
+    var blobAdapter = require('skipper-s3')({
+      bucket: process.env.BUCKET,
+      key: process.env.KEY,
+      secret: process.env.SECRET
+    });
+    var receiving = blobAdapter.receive();
+
+    req.file('avatar').upload(receiving, function whenDone(err, uploadedFiles) {
+      if (err) {
+        return res.serverError(err);
+      }
+      else return res.json({
+        files: uploadedFiles,
+        textParams: req.params.all()
+      });
+    });
+  },
+
 
   /**
    * FileController.download()
