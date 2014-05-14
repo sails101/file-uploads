caf4f4cda7baf120307d01aefd134a1fce5f71d0
Mike McNeil <mike@balderdash.co>
2014-05-14 12:05:24 -0500

    Added maxbytes to disk upload

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -24,7 +24,12 @@ module.exports = {
     // Node defaults to 2 minutes.
     res.setTimeout(0);
 
-    req.file('avatar').upload(function whenDone(err, uploadedFiles) {
+    req.file('avatar')
+    .upload({
+
+      // You can apply a file upload limit (in bytes)
+      maxBytes: 1000000
+    }, function whenDone(err, uploadedFiles) {
       if (err) return res.serverError(err);
       else return res.json({
         files: uploadedFiles,
